39071fc7e294c2888b3e631b92269e06760608da
HJJ1007 <HJJ1007@users.noreply.github.com>
2021-01-25 23:02:08 +0900

    닷지 게임 완성

--- Dodge/Assets/Scripts/PlayerController.cs
+++ Dodge/Assets/Scripts/PlayerController.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody PlayerRigidbody;
-    public float speed = 8f;
+    [SerializeField] private float speed = 8f;
     private void Start()
     {
         // 게임 오브젝트에서 Rigidbody 컴포넌트를 찾아 playerRigidbody에 할당
         PlayerRigidbody = GetComponent<Rigidbody>();
     }
-    private void Update()
+    private void FixedUpdate()
     {
         // 수평축과 수직축의 입력값을 감지하여 저장
         float xInput = Input.GetAxis("Horizontal");
@@ -23,26 +23,17 @@ public class PlayerController : MonoBehaviour
         Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
         // 리지드바디의 속도에 newVelocity 할당
         PlayerRigidbody.velocity = newVelocity;
-
-        //if (Input.GetKey(KeyCode.UpArrow) == true)
-        //{
-        //    PlayerRigidbody.AddForce(0f, 0f, speed);
-        //}
-        //if (Input.GetKey(KeyCode.DownArrow) == true)
-        //{
-        //    PlayerRigidbody.AddForce(0f, 0f, -speed);
-        //}
-        //if (Input.GetKey(KeyCode.LeftArrow) == true)
-        //{
-        //    PlayerRigidbody.AddForce(-speed, 0f, 0f);
-        //}
-        //if (Input.GetKey(KeyCode.RightArrow) == true)
-        //{
-        //    PlayerRigidbody.AddForce(speed, 0f, 0f);
-        //}
+    }
+    private void Update()
+    {
+        
     }
     public void Die()
     {
         gameObject.SetActive(false);
+        // 씬에 존재하는 GameManager 타입의 오브젝트를 찾아서 가져오기
+        GameManger gameManager = FindObjectOfType<GameManger>();
+        // 가져온 GameManager 오브젝트의 EndGame() 메서드 실행
+        gameManager.EndGame();
     }
 }
