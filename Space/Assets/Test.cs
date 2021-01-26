b3467c4911a2030bb7205192e54544d2aa439fde
HJJ1007 <HJJ1007@users.noreply.github.com>
2021-01-26 22:02:36 +0900

    study

---
+++ Space/Assets/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    Rigidbody myRigidbody;
+    Vector3 rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        rotation = transform.rotation.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rotation += Vector3.right * 90 * Time.deltaTime;
+            myRigidbody.MoveRotation(Quaternion.Euler(rotation));
+        }
+    }
+}
